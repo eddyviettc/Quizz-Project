4f231935de9a6c9fabc63410f1d41191edf1c24c
Eddy Viet <eddyviettc@gmail.com>
2024-05-30 16:47:02 +0700

    handling detail quiz data from backen by using lodash

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react"
 import { useParams } from "react-router-dom"
 import { getDataQuiz } from "../../services/apiService"
+import _ from 'lodash'
 const DetailQuiz = (props) => {
     const params = useParams()
     const quizId = params.id
@@ -10,6 +11,30 @@ const DetailQuiz = (props) => {
     const fetchQuestions = async () => {
         let res = await getDataQuiz(quizId)
         console.log('check quess ', res)
+        if (res && res.EC === 0) {
+            let raw = res.DT
+            let data = _.chain(raw)
+                // Group the elements of Array based on `color` property
+                .groupBy("id")
+                // `key` is group's name (color), `value` is the array of objects
+                .map((value, key) => {
+                    let answers = []
+                    let questionDescription, image = null
+                    value.forEach((item, index) => {
+                        if (index === 0) {
+                            questionDescription = item.description
+                            image = item.image
+                        }
+                        answers.push(item.answers)
+
+                    })
+
+                    return { questionId: key, answer: answers, questionDescription, image }
+                }
+                )
+                .value()
+            console.log(data)
+        }
     }
     console.log('check params', params)
     return (
