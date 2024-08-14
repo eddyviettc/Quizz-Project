55be37c211ee4a63fa6088e0e6978ee5c86253ae
Eddy Viet <eddyviettc@gmail.com>
2024-08-14 17:17:50 +0700

    design base css for question right content

--- src/components/User/Question.js
+++ src/components/User/Question.js
@@ -1,7 +1,10 @@
 import _ from 'lodash'
+import { useState } from 'react';
+import Lightbox from "react-awesome-lightbox";
 
 const Question = (props) => {
     const { data, index } = props;
+    const [isPreviewImage, setIsPreviewImage] = useState(false)
     if (_.isEmpty(data)) {
         return (<></>)
     }
@@ -14,7 +17,21 @@ const Question = (props) => {
         <>
             {data.image ?
                 <div className='q-image'>
-                    <img src={`data:image/jpeg;base64,${data.image}`} />
+                    <img
+                        style={{ cursor: 'pointer' }}
+                        src={`data:image/jpeg;base64,${data.image}`}
+                        onClick={() => setIsPreviewImage(true)}
+
+                    />
+                    {isPreviewImage === true &&
+                        <Lightbox
+                            image={`data:image/jpeg;base64,${data.image}`}
+                            title={'Question Image'}
+                            onClose={() => setIsPreviewImage(false)}
+
+
+                        ></Lightbox>
+                    }
                 </div>
                 :
                 <div className='q-image'>
